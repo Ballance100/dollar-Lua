2e4ae2929b6cb51d1e5d039f78ed17f928cb8747
Ballance100 <calumballance994@outlook.com>
2022-02-26 00:36:15 +0000

    Now works as a lib

---
+++ file.lua
@@ -0,0 +1,7 @@
+objects = objects + 1
+ objects = objects + 1
+ Obj = Obj + 1
+ ob = 5
+ print(Obj[#Obj])
+ print(Obj.file[#Obj.file])
+ Obj.file
--- init.lua
+++ init.lua
@@ -11,36 +11,49 @@ transpiledScript = ""
 
 local lastVar = ""
 
-local function findLastVariable(variableEndingPoint,fullScript)
-    lastVar = ""
-    local ready = false
-    for i=variableEndingPoint,1, -1 do--Var can be max of 999999 chars long
+local main = {} --contains all funcs
 
-        c = fullScript:sub(i,i)
-
-        if not c:find("%a") and c ~= "." and ready then lastVar = lastVar break end
-
-        if c:find("%a") or c == "." then  ready = true lastVar = lastVar .. c end
-        --c:find checks if it's a var
-    end
-    lastVar = string.reverse(lastVar)
-end
-
-for i = 1, #script do
-    local c = script:sub(i,i)
-    if c == "$" then
-        if lastVar == "" then error "$ in wrong position" end
-        transpiledScript = transpiledScript .. lastVar
+ main.transpile = function(script)
+    local function findLastVariable(variableEndingPoint,fullScript)
         lastVar = ""
+        local ready = false
+        for i=variableEndingPoint,1, -1 do--Var can be max of 999999 chars long
 
-    elseif c == "=" or "[" then
-        transpiledScript = transpiledScript .. c
-        findLastVariable(i,script)
-    else
+            c = fullScript:sub(i,i)
 
-        transpiledScript = transpiledScript .. c
+            if not c:find("%a") and c ~= "." and ready then lastVar = lastVar break end
+
+            if c:find("%a") or c == "." then  ready = true lastVar = lastVar .. c end
+            --c:find checks if it's a var
+        end
+        lastVar = string.reverse(lastVar)
     end
+
+    for i = 1, #script do
+        local c = script:sub(i,i)
+        if c == "$" then
+            if lastVar == "" then error "$ in wrong position" end
+            transpiledScript = transpiledScript .. lastVar
+            lastVar = ""
+
+        elseif c == "=" or "[" then
+            transpiledScript = transpiledScript .. c
+            findLastVariable(i,script)
+        else
+
+            transpiledScript = transpiledScript .. c
+        end
+    end
+
+    print("\n Old Script:"..script.."\n\nNew Script: "..transpiledScript)
+    return transpiledScript
 end
 
+main.transpileAndSave = function(script,dir)
+    local file = io.open(dir,"w+")
+    io.output(file)
+    io.write(main.transpile(script))
+    file:close()
+end
 
-print("\n Old Script:"..script.."\n\nNew Script: "..transpiledScript)
+main.transpileAndSave(script,"file.lua")
